ac3a62623fa6d5ed434252f7a0f58a421bd8345c
GABRIEL HENRIQUE DOS SANTOS NEPOMUCENO <gabriel.hsnepomuceno@senacsp.edu.br>
2026-03-17 16:22:29 -0300

    Tela1.1

---
+++ TelaLogin/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace TelaLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            linkLabel1 = new LinkLabel();
+            checkBox1 = new CheckBox();
+            textBox2 = new TextBox();
+            linkLabel2 = new LinkLabel();
+            rButton1 = new RButton();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            label1 = new Label();
+            pictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.None;
+            textBox1.Location = new Point(847, 456);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Usuario";
+            textBox1.Size = new Size(297, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.ActiveLinkColor = Color.SteelBlue;
+            linkLabel1.Anchor = AnchorStyles.None;
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = SystemColors.ActiveCaptionText;
+            linkLabel1.ForeColor = Color.Gold;
+            linkLabel1.LinkColor = Color.Gold;
+            linkLabel1.Location = new Point(1036, 541);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(90, 15);
+            linkLabel1.TabIndex = 2;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Esqueci a senha";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // checkBox1
+            // 
+            checkBox1.Anchor = AnchorStyles.None;
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = Color.Black;
+            checkBox1.ForeColor = Color.Gold;
+            checkBox1.Location = new Point(828, 541);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(92, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Lembrar-me";
+            checkBox1.UseVisualStyleBackColor = false;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.None;
+            textBox2.Location = new Point(847, 495);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Senha";
+            textBox2.Size = new Size(297, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.ActiveLinkColor = Color.SteelBlue;
+            linkLabel2.Anchor = AnchorStyles.None;
+            linkLabel2.AutoSize = true;
+            linkLabel2.BackColor = SystemColors.ActiveCaptionText;
+            linkLabel2.ForeColor = SystemColors.ActiveCaptionText;
+            linkLabel2.LinkColor = Color.Gold;
+            linkLabel2.Location = new Point(828, 613);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(73, 15);
+            linkLabel2.TabIndex = 5;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Cadastrar-se";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // rButton1
+            // 
+            rButton1.Anchor = AnchorStyles.None;
+            rButton1.BackColor = Color.Gold;
+            rButton1.BackgroundColor = Color.Gold;
+            rButton1.BorderColor = Color.PaleVioletRed;
+            rButton1.BorderRadius = 20;
+            rButton1.BorderSize = 0;
+            rButton1.FlatAppearance.BorderSize = 0;
+            rButton1.FlatStyle = FlatStyle.Flat;
+            rButton1.ForeColor = SystemColors.ActiveCaptionText;
+            rButton1.Location = new Point(828, 570);
+            rButton1.Name = "rButton1";
+            rButton1.Size = new Size(316, 40);
+            rButton1.TabIndex = 8;
+            rButton1.Text = "Entrar";
+            rButton1.TextColor = SystemColors.ActiveCaptionText;
+            rButton1.UseVisualStyleBackColor = false;
+            rButton1.Click += rButton1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(810, 456);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(29, 20);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.None;
+            pictureBox2.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(810, 495);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(30, 23);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 10;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Anchor = AnchorStyles.None;
+            pictureBox3.BackColor = Color.Black;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(828, 263);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(316, 140);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 11;
+            pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Gold;
+            label1.Location = new Point(1713, 1047);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 12;
+            label1.Text = "Powered by";
+            label1.Click += label1_Click_1;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(1724, 985);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(256, 120);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 13;
+            pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1920, 1080);
+            Controls.Add(label1);
+            Controls.Add(linkLabel2);
+            Controls.Add(pictureBox1);
+            Controls.Add(rButton1);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(linkLabel1);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox4);
+            ForeColor = Color.Yellow;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private LinkLabel linkLabel1;
+        private CheckBox checkBox1;
+        private TextBox textBox2;
+        private LinkLabel linkLabel2;
+        private RButton rButton1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label label1;
+        private PictureBox pictureBox4;
+    }
+}
---
+++ TelaLogin/Form1.cs
@@ -0,0 +1,55 @@
+namespace TelaLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void rButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
